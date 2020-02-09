0f1e017f72bfb834b70349d44467ba30e13dad04
Liu Yi <1369021134@qq.com>
2020-02-09 17:25:49 +0800

    部署

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,4 @@
-sResult='djafjjdjaf的发生顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶jsdk'
+sResult='djafjjdjaf二万人的发生顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶jsdk'
 with open("./911.txt", "w", encoding='utf-8') as f:
     f.write(str(sResult))
     f.close()
